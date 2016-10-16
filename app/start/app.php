50c6ecb782b1bff66091383cc918d5eb6e55f15d
gerson22 <gerson.isaias.rivera@gmail.com>
2016-10-16 15:16:19 -0500

    La carga de las vistas ya se hace desde aqui

--- app/start/app.php
+++ app/start/app.php
@@ -5,7 +5,18 @@ class app{
 
     }
     public static function run(){
+
+        /*--------------------------------------
+        Requerimos el autoload del vendor con la
+        intención de poder utilizar la librería
+        TWIG que nos servira para la manipulación
+        de plantillas (Herencia, renderización,
+        carga con datos, e.t.c)
+        ----------------------------------------*/
+        require_once 'vendor/autoload.php';
+
         require_once __DIR__.'/../routes.php';
+
     }
 
 
