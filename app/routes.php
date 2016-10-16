89e37e697ffc452723c455ac2c15faa4979b6062
gerson22 <gerson.isaias.rivera@gmail.com>
2016-10-16 15:15:46 -0500

    Cambio de ruta ejemplo

--- app/routes.php
+++ app/routes.php
@@ -6,11 +6,8 @@
         personalizadas el segundo paramentro nos dira
         su callback o a que controlador ira
     */
-    Router::get('/getUser','User:get');
-    /*Router::add('/.*', function () {
-        header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
-        echo '<h1>404 - El sitio solicitado no existe</h1>';
-    });*/
+    Router::get('/','homeCtrl:home');
+
     Router::Error404(function () {
         //Obtiene el error 404 para personalizarlo
         echo '<h1>404 - El sitio solicitado no existe</h1>';
